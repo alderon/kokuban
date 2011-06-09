db4ef9ea2260dce29721ac042c7f5fccda4df306
Roel van Dijk <github2@rustradio.org>
2011-06-09 21:34:02 +0200

    Fix integration test, some more eyecandy.

--- db/evolutions/1.sql
+++ db/evolutions/1.sql
@@ -8,7 +8,8 @@ CREATE TABLE Fragment (
     body text NOT NULL,
     style varchar(255) NOT NULL,
     created_at datetime NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FULLTEXT (title, body)
 );
  
 # --- !Downs
